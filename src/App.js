e893faab29da0d82bf93f48195c3547e241e5d27
maa11235 <165483888+maa11235@users.noreply.github.com>
2025-08-13 02:27:35 -0600

    Update URL

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   const handleGenerate = async () => {
     try {
-      const response = await fetch("http://api.meal-planner.techexamprep.com/generate-meal-plan", {
+      const response = await fetch("https://api.meal-planner.techexamprep.com/generate-meal-plan", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
