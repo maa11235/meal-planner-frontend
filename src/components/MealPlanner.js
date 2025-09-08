18944923581ebe5562509ae1d81db10e6706d608
maa11235 <165483888+maa11235@users.noreply.github.com>
2025-09-07 19:25:03 -0600

    Add snacks and desert to meal type drop down

--- src/components/MealPlanner.js
+++ src/components/MealPlanner.js
@@ -26,6 +26,8 @@ export default function MealPlanner() {
         <option value="breakfast">Breakfast</option>
         <option value="lunch">Lunch</option>
         <option value="dinner">Dinner</option>
+        <option value="snacks">Snack</option>
+        <option value="dessert">Dessert</option>
       </select>
       <button onClick={handleGenerate}>Generate Meal Plan</button>
 
